7b6b4bb17a1c4eac83ba19cd59094fa7754e993a
priyanshsingh <singh.priyansh1391@gmail.com>
2025-04-16 18:50:18 +0530

    vr

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import './App.css'
+import Navbaar from './components/navbar'
+import HeroSection from './components/heroSection'
+import FeatureSection from './components/FeatureSection'
+import Workflowsection from './components/Workflowsection'
+import Pricing from './components/Pricing'
+import Testimonials from './components/Testimonials'
+import Footer from './components/Footer'
+ 
+
+
+function App() {
+ 
+ 
+  return (
+    <>
+    
+      <div>
+      <Navbaar />
+      <div className="max-w-7xl mx-auto pt-20 px-6 text-center justify-center">
+      <HeroSection/>
+      <FeatureSection/>
+      <Workflowsection />
+      <Pricing/>
+      <Testimonials/>
+      <Footer/>
+
+      </div>
+       </div>
+    </>
+  )
+}
+
+export default App
